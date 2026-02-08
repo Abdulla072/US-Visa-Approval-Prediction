0e5540e340c6ad57ec04fa822ea9305698da5f98
Abdulla Al Biswas <abdullaalbiswas@gmail.com>
2026-02-09 00:02:08 +0600

    folder structure added

---
+++ template.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+Project_name="US_Visa"
+
+List_of_files=[
+    f"{Project_name}/__init__.py",
+    f"{Project_name}/components/__init__.py",
+    f"{Project_name}/components/data_ingestion.py",
+    f"{Project_name}/components/data_validation.py",
+    f"{Project_name}/components/data_transformation.py",
+    f"{Project_name}/components/model_trainer.py",
+    f"{Project_name}/components/model_evaluation.py",
+    f"{Project_name}/components/model_pusher.py",
+    f"{Project_name}/configuration/_init_.py",
+    f"{Project_name}/constants/__init__.py",
+    f"{Project_name}/entity/__init__.py",
+    f"{Project_name}/config_entity.py",
+    f"{Project_name}/entity/artifact_entity.py",
+    f"{Project_name}/exception/__init__.py",
+    f"{Project_name}/logger/__init__.py",
+    f"{Project_name}/pipeline/__init__.py",
+    f"{Project_name}/training_pipeline.py",
+    f"{Project_name}/prediction_pipeline.py",
+    f"{Project_name}/utils/__init__.py",
+    f"{Project_name}/utils/main_utils.py",
+    
+    "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml",
+    
+]
+
+for filepath in List_of_files:
+    filepath=Path(filepath)
+    print(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        if(not os.path.exists(filepath) or os.path.getsize(filepath)==0):
+            with open(filepath,'w') as f:
+                pass
+        else:
+            print(f"{filename} is already in {filedir} and has some content.Skipping creation.")
+            
+    
