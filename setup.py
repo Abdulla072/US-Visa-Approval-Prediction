6edcd74bdd099fc1efb162a688d757d49336cf8f
Abdulla Al Biswas <abdullaalbiswas@gmail.com>
2026-02-09 11:47:55 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa",
+    version="0.0.1",
+    author="Abdulla Al Biswas",
+    author_email="abdullaalbiswas@gmail.com",
+    description="US Visa Approval Prediction ML Project",
+    packages=find_packages(),
+)
