7e19dc4e045a46065fbd85d011a45e40a93b710c
Abdulla Al Biswas <abdullaalbiswas@gmail.com>
2026-02-11 23:24:24 +0600

    Updated exception handling and demo file

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
 from US_Visa .logger import logging
+from US_Visa.exception import USVisaException
 logging.info("This is a test log message.")
+import sys
+
+
+
+try:
+    r=3/0
+    print(r)
+except Exception as e:
+    raise USVisaException(e, sys)
